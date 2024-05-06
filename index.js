aa2a9a61fdc7499cceb3d15ad68bd4c013968bd5
cleissongomes <cleissongomes777@gmail.com>
2024-05-06 10:17:40 -0300

    feat(routes): add POST route to pedidos.routes

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 import express from 'express';
 import route from './routes/pedidos.routes.js';
+import { promises as fs } from 'fs';
+
+const { readFile, writeFile } = 'fs';
 
 const app = express();
 app.use(express.json());
 app.use(route);
 
-app.listen(3000, (req, res) => {
-  console.log('Api Started!');
+app.listen(5000, async () => {
+  try {
+    console.log('Api Started!');
+    await readFile('./pedidos.json');
+  } catch (err) {
+    const initialJson = {
+      nextId: 501,
+      pedidos: [],
+    };
+  }
 });
--- routes/pedidos.routes.js
+++ routes/pedidos.routes.js
@@ -1,4 +1,45 @@
 import express from 'express';
+import { promises } from 'fs';
 const route = express.Router();
+
+const { readFile, writeFile } = 'fs';
+
+route.post('/pedidos', async (req, res) => {
+  try {
+    let pedidos = req.body;
+    const data = JSON.parce(await readFile('pedidos.json'));
+
+    if (!pedidos.cliente || typeof pedidos.cliente !== 'string') {
+      res
+        .status(400)
+        .send({ error: 'O campo cliente precisa ser preenchido.' });
+    }
+    if (!pedidos.produto || typeof pedidos.produto !== 'string') {
+      res
+        .status(400)
+        .send({ error: 'O campo pedidos precisa ser preenchido.' });
+    }
+    if (!pedidos.valor || typeof pedidos.valor !== 'number') {
+      res.status(400).send({ error: 'O campo valor precisa ser preenchido.' });
+    }
+    if (!pedidos.entregue || typeof pedidos.entregue !== 'string') {
+      res
+        .status(400)
+        .send({ error: 'O campo entregue precisa ser preenchido.' });
+    }
+
+    let timeStamp = new Date();
+    pedidos.timeStamp = timeStamp;
+
+    pedidos = { id: data.nextId++, ...pedidos };
+    data.pedidos.push(pedidos);
+
+    await writeFile('pedidos.json', JSON.stringify(data));
+
+    res.send(pedidos);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 export default route;
